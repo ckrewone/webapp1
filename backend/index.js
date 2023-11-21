79f5f68f78f2f7dc150a9d779dcce881ee03f946
Michał Żakowski <ckrewone@o2.pl>
2023-11-21 09:48:10 +0000

    Added StartView and handled call action and chceking call status

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express  = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const config = {
  url: 'https://uni-call.fcc-online.pl',
@@ -9,6 +11,14 @@ const config = {
 };
 
 dialer.configure(config); 
+
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+ next();
+});
 
 // Serwer nasłuchuje na porcie 3000
 httpServer.listen(3000, function () {
@@ -21,3 +31,31 @@ httpServer.get('/call/:number1/:number2', (req, res) => {
  dialer.call(number1,number2);
  res.json({success: true});
 })
+
+httpServer.post('/call/', async (req, res) => {
+    const number1 = req.body.number;
+    const number2 = '519596016' // tutaj dejemy swój numer
+    console.log('Dzwonie', number1, number2)
+    const bridge = await dialer.call(number1, number2);
+    let interval = setInterval(async () => {
+      let status = await bridge.getStatus();
+      console.log(status)
+      if (
+        status === "ANSWERED" ||
+        status === "FAILED" ||
+        status === "BUSY" ||
+        status === "NO ANSWER"
+      ) {
+        console.log("stop");
+        clearInterval(interval);
+      }
+    }, 2000);
+    res.json({ success: true });
+   })
+   
+
+   httpServer.get('/status', async function (req, res) {
+    let status = await bridge.getStatus();
+    res.json({ id: '123', "status": status });
+   })
+   
